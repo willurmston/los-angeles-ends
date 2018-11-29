162fa87c36323d4cb94e0e8c66918a1c81f6c050
Will Urmston <wurmston@risd.edu>
2018-11-28 16:47:09 -0800

    Refine dither.sh

--- dither.sh
+++ dither.sh
@@ -6,12 +6,15 @@
 # brew install netpbm
 
 # TO USE, RUN THIS COMMAND:
-# sh dither.sh input-file.mov
+# sh dither.sh content/song-slug/my-input-video.mov "#65008A"
+
 
 # Make a nicely named variable from the first argument
 INPUT=$1
+# Color that will replace black in each frame
+BLACK_COLOR=$2
 # Adjust gamma of output (1-2, 2 = very bright)
-GAMMA=1.2
+GAMMA=1.4
 # Target framerate
 RATE=12
 # The height of the output video, in pixels
@@ -26,22 +29,29 @@ ffmpeg -i $INPUT -r $RATE -vf "scale=-2:$SIZE:flags=neighbor, pp=al, eq=gamma=$G
 
 cd frames
 
+# OLD: Create color palette for ImageMagick
+# convert xc:$BLACK_COLOR xc:"#ffe6c2" +append palette.png
+
+# Create a color palette for FFMPEG
+convert xc:$BLACK_COLOR xc:"#ffe6c2" +append \
+    -interpolate Nearest -rotate "-90" -filter point -resize "16x16!"  palette.png
+
 # For each frame:
-for i in *.png; do
+for i in img*.png; do
     # Convert to grayscale,                  Dither               Write to PNG
     convert $i -grayscale Rec709Luma pgm:- | pamditherbw -floyd | pnmtopng > p$i;
-    # convert $i -grayscale Rec709Luma pgm:- | pamditherbw -floyd | convert - -fuzz 50% -fill '#000000' -opaque "#C62529" pam:- | pnmtopng > p$i;
+    # OLD: Restrict colors to previously-created color palette with ImageMagick
+    # convert p$i -size 1!x$SIZE palette.png -clut p$i;
 done;
 
-# GOOD!
-ffmpeg -i pimg%5d.png -i ../$INPUT -map 0:0? -map 1:1? -preset veryslow -codec:a libfdk_aac -b:a 128k -codec:v h264 -pix_fmt yuv420p -b:v 7M -maxrate 26M -bufsize 0.5M -r $RATE -tune stillimage -sws_flags neighbor ../dithered-$INPUT.mp4
+# FFMPEG Flag notes
+# -crf (default 22) sets a constant bitrate factor (results in larger filesizes than necessary, but ensures quality level). Not what we want. we start to see artifacts above 22
+# -preset (default medium) Slower values take longer but yield higher compression (same quality). Use `veryslow`.
+# -pix_fmt Pixel format (`yuv420p` is best for web. Has widest browser support).
+# -tune Optimizes the codec for different media. `stillimage` is optimized for unique frames
 
-# No artifacts, but starts to show some gray pixels. Maybe thats ok.
-# 3.6MB
-# ffmpeg -i pimg%5d.png -i ../$INPUT -map 0:0? -map 1:1? -preset veryslow -codec:a libfdk_aac -b:a 128k -codec:v h264 -pix_fmt yuv420p -b:v 7M -maxrate 24M -bufsize 0.5M -r $RATE -tune stillimage -sws_flags neighbor ../dithered-$INPUT.mp4
-
-# ffmpeg -i pimg%5d.png -i ../$INPUT -map 0:0? -map 1:1? -preset veryslow -codec:a libfdk_aac -b:a 128k -codec:v h264 -pix_fmt yuv420p -b:v 7M -maxrate 24M -bufsize 0.2M -r $RATE -tune stillimage -sws_flags neighbor ../dithered-$INPUT.mp4
+cd ..
+ffmpeg -i frames/pimg%5d.png -i frames/palette.png -lavfi "paletteuse=dither=floyd_steinberg:alpha_threshold=0" -preset veryslow -codec:v h264 -pix_fmt yuv420p -b:v 20M -maxrate 30M -bufsize 1M -r $RATE -tune stillimage -sws_flags neighbor -y $INPUT.mp4
 
 # Delete frame files
-cd ..
 rm -rf frames
