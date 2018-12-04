2304896722ddfb281fb6a520782d4a66a608890a
Will Urmston <wurmston@risd.edu>
2018-12-03 16:35:31 -0800

    Toolbar and PlayButton desktop styles

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -13,13 +13,30 @@ const style = css`
     z-index: 100;
     transition: transform 0.3s 1s;
     will-change: transform;
+    @media screen and (min-width: 600px) {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        overflow: hidden;
+        top: auto;
+        bottom: 0;
+        height: 70px;
+    }
     &.hidden {
         transition: transform 0.3s;
         transform: translateY(-100%);
+        @media screen and (min-width: 600px) {
+            transform: translateY(100%);
+        }
     }
     & h2 {
         margin: 14px 10px 6px 10px;
         letter-spacing: 0.05em;
+        @media screen and (min-width: 600px) {
+            flex-shrink: 0;
+            font-size: 30px;
+            margin: 0 auto 0 16px;
+        }
     }
     & nav {
         width: calc(100% - 100px);
@@ -27,6 +44,11 @@ const style = css`
         overflow: hidden;
         flex-wrap: no-wrap;
         margin-left: 10px;
+        @media screen and (min-width: 600px) {
+            flex-shrink: 1;
+            margin-right: 24px;
+            max-width: 45vw;
+        }
         & div {
             margin: 0 4px 0 0;
             height: 3px;
@@ -34,6 +56,11 @@ const style = css`
             flex-shrink: 1;
             transition: opacity 0.3s;
             background: var(--song-color);
+            @media screen and (min-width: 600px) {
+                margin-right: 0;
+                margin-left: 8px;
+                height: 8px;
+            }
         }
         & div.past {
             opacity: 0.5;
@@ -54,6 +81,18 @@ const style = css`
         &:active {
             opacity: 0.5;
         }
+        @media screen and (min-width: 600px) {
+            position: static;
+            flex-shrink: 0;
+            margin-left: 14px;
+            width: 48px;
+            height: 48px;
+            transition: opacity 0.05s;
+            &:hover {
+                cursor: pointer;
+                opacity: 0.8;
+            }
+        }
     }
 `
 
@@ -68,22 +107,44 @@ export default class Toolbar extends Component {
             buttonIcon = 'next'
         }
 
-        return (
-            <div
-                class={cx('Toolbar', style, this.props.isOpen ? '' : 'hidden' )}
-            >
-                <h2>{this.props.song.title.toUpperCase()}</h2>
-                <nav>
-                    {this.props.song.slides.map( (slide, index) =>
-                        <div class={index < this.props.currentSlideIndex && 'past'}></div>
-                    )}
-                </nav>
-                <PlayButton
-                    icon={buttonIcon}
-                    progress={this.props.playerProgress}
-                    onclick={this.props.onPlayButtonClick}
-                />
-            </div>
-        )
+        const bigScreen = window.matchMedia('screen and (min-width: 600px)').matches
+
+        if (bigScreen) {
+            return (
+                <div
+                    class={cx('Toolbar', style, this.props.isOpen ? '' : 'hidden' )}
+                >
+                    <PlayButton
+                        icon={buttonIcon}
+                        progress={this.props.playerProgress}
+                        onclick={this.props.onPlayButtonClick}
+                    />
+                    <h2>{this.props.song.title.toUpperCase()}</h2>
+                    <nav>
+                        {this.props.song.slides.map( (slide, index) =>
+                            <div class={index < this.props.currentSlideIndex && 'past'}></div>
+                        )}
+                    </nav>
+                </div>
+            )
+        } else {
+            return (
+                <div
+                    class={cx('Toolbar', style, this.props.isOpen ? '' : 'hidden' )}
+                >
+                    <h2>{this.props.song.title.toUpperCase()}</h2>
+                    <nav>
+                        {this.props.song.slides.map( (slide, index) =>
+                            <div class={index < this.props.currentSlideIndex && 'past'}></div>
+                        )}
+                    </nav>
+                    <PlayButton
+                        icon={buttonIcon}
+                        progress={this.props.playerProgress}
+                        onclick={this.props.onPlayButtonClick}
+                    />
+                </div>
+            )
+        }
     }
 }
