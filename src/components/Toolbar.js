5e5c2f1b24e006b16bee8b43fe7ae80bbc9a66ae
Will Urmston <wurmston@risd.edu>
2018-12-05 14:03:16 -0800

    Make Toolbar slide in faster

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -11,7 +11,7 @@ const style = css`
     color: var(--song-color);
     width: 100%;
     z-index: 100;
-    transition: transform 0.3s 1s;
+    transition: transform 0.3s 0.2s;
     will-change: transform;
     @media screen and (min-width: 600px) {
         display: flex;
