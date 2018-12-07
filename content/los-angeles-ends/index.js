63f81e867bdde3974f920eb17f6379db838aca92
simulcast <thisissimulcast@gmail.com>
2018-12-06 19:29:13 -0800

    Update content/los-angeles-ends/index.js

--- content/los-angeles-ends/index.js
+++ content/los-angeles-ends/index.js
@@ -30,6 +30,16 @@ module.exports = {
                 as the pitch comes in at 3 - 0
 
                 this is the light that will take us with it
+
+                [i can see clearly now the rain is gone
+
+                i can see all obstacles in my way
+
+                gone are the dark clouds that had me blind
+
+                it's gonna be a bright, bright sun-shining day
+                
+                it's gonna be a bright, bright sun-shining day]
             `,
         },
         {
@@ -37,7 +47,7 @@ module.exports = {
             markdown: `
                 december 2016 and the sun, set but still glowing over glendale, radiates off the hood of my car. a pulsating orange rips through the sky, indexing every time i’ve seen los angeles play the apocalypse’s backdrop.
 
-                i think about childhood nightmares of nuclear bombs exploding on the horizon, of knowing the end is soon and near but not right now or quite yet here. i think about miracle mile, a movie that my parents showed me when i was too young, where missiles come flying over the santa monica mountains and land in the la brea tar pits. i think about the episode of 24 where jack bauer cries as a mushroom cloud rises over valencia.
+                i think about childhood nightmares of nuclear bombs exploding on the horizon, of knowing the end is soon and near but not right now or quite yet here. i think about miracle mile, a movie that my parents showed me when i was too young to process death properly, where missiles come flying over the santa monica mountains and land in the la brea tar pits. i think about the episode of 24 where jack bauer cries as a mushroom cloud rises over valencia.
 
                 sunset in los angeles always confuses the day’s end and the end of days.
             `,
@@ -45,6 +55,59 @@ module.exports = {
         {
             type: 'video',
             file: require('./miracle-mile.mp4')
+        },
+        {
+            type: 'narration',
+            markdown: `
+                ever since my mom died, october 2009, while the station fire burned in the san gabriels, i've been having nightmares set atop elysian park.
+
+i can see a third of the playing field at dodger stadium, and i’ve taken a picnic and a transistor radio to follow the game. santa ana winds cut the air. then i turn my head and watch as the light grows closer, warmer, brighter.
+
+“i guess that’s it,” i think.
+
+i wake up in a sweat. it's my fault — it wouldn’t have happened if i hadn’t looked.
+            `
+        },
+        {
+            type: 'video',
+            file: require('./scully-sun.mp4')
+        },
+        {
+            type: 'narration',
+            markdown: `
+                also in 2016, my dad wrote a short story following an eye surgery that split his vision, temporarily, in two. in it, los angeles ends, downtown disappearing in an orange burst. we never spoke to each other about what we were working on.
+            `
+        },
+        {
+            type: 'quote',
+            markdown: `
+            "I was driving east across Sunset. Here and there the familiar buildings of downtown – most prominently, the Library Tower – swung into view behind the hills and the haze. And then though left (“blue”) eye I saw downtown, all of it—  Disappear. As if it had never been.  Obliterated in a flash of light more intense than can be imagined or described. The canonical phrase only hints at what I (monocularly) saw. “Brighter than a thousand suns.”
+
+            I know what happened to downtown and I know what that means. The only thing I do not know is why I, perhaps alone, was given the privilege – if one might call it that – of seeing it before it occurred."
+            – Howard A. Rodman, "BANG", Black Clock
+            `
+        },
+        {
+            type: 'narration',
+            markdown: `
+            it's 2018 now and i'm revisiting this song as malibu west valley are on fire, uncontained. when i wrote it, depressed by deffered grief, both personal and societal, i was fascinated with the world ending. i was excited, even, by the possibility.
+
+i fantasized about los angeles ending because i was ending.
+
+because loss makes the apocalypse feel like an appealing alternative to life.
+            `   
+        },
+        {
+            type: 'narration',
+            markdown: `
+            i drive the 134 as the woolsey fire burns, sun blaring orange through the haze. radiant, beautiful, and terrifying — the exact same shade i wrote this song about. but there's nothing exciting about it at all. i feel instead a sorrow, deep and painful, ripping me from myself.
+
+            i don't want this.
+
+            over the summer, j suggested to me that i use "i can see clearly now" as a sample in a simulcast live set. it reads so differently, he said, when you think about it in terms of climate change. it's haunting, prescient.
+
+            i cut the sample in the same day the woolsey fire is finally contained.
+            `   
         },
         {
             type: 'quote',
@@ -57,28 +120,12 @@ module.exports = {
         {
             type: 'narration',
             markdown: `
-                i have a nightmare that plays every month where i’m sitting atop elysian park. i can see a third of the playing field at dodger stadium, and i’ve taken a picnic and a transistor radio to follow the game. santa ana winds cool the air. then i turn my head to the right and watch as the light grows closer, warmer, brighter.
+            there's something instructive about coming so close to the end and deciding that it's not the way forward. that the world is ending but you are not.
 
-                “i guess that’s it,” i think.
+            on the best days it feels obvious, on the worst it feels like a lie.
 
-                i wake up in a sweat. it's my fault — it wouldn’t have happened if i hadn’t looked.
-            `
-        },
-        {
-            type: 'video',
-            file: require('./scully-sun.mp4')
-        },
-        {
-            type: 'narration',
-            markdown: `
-                loss often makes the apocalypse feel like an appealing alternative to life.
-
-                a ripping feeling, being pulled apart, a desire for the end because it might return you to the person who left — the end of the world is deeply personal. no surprise that after my mom died i dreamt repeatedly of los angeles in flames. of it exploding, expanding, ending. i didn't want to live.
-
-                there's something instructive about coming so close to the end and deciding that it's not the way forward. that the world is ending but you are not. on the best days it feels obvious, on the worst it feels like a lie.
-
-                not an explosion, just a tear in two.
-            `
+            not a fire, not an explosion, just a constant tear in two.
+            `   
         }
     ]
 }
