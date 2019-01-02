5915dbd6ba21129ff2d2254d6a5b8d870b413644
Will Urmston <wurmston@risd.edu>
2019-01-02 13:09:57 -0800

    Add method to get an element's position relative to document

--- src/utils.js
+++ src/utils.js
@@ -2,5 +2,11 @@ export default {
     cycleArray: (array, amount, l=array.length) => {
         const offset = (((amount % l) + l) %l)
         return array.slice(offset).concat(array.slice(0,offset))
+    },
+    offset: el => {
+        const rect = el.getBoundingClientRect()
+        const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
     }
 }
