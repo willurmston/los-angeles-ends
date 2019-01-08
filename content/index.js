f33004ec89f5f2dd3c5622da278b525327d91445
simulcast <thisissimulcast@gmail.com>
2019-01-08 10:53:58 -0800

    liner note updates

--- content/index.js
+++ content/index.js
@@ -37,26 +37,24 @@ module.exports = {
     ],
     // Use asterisks around *someone's name* to make it pink
     credits: markdown`
-        ## MUSIC
 
         All research, writing, performance, arrangement, engineering, production, and mixing by *[Tristan Rodman](www.tristanrodman.com)* unless noted here.
 
-        Tracks 2, 3, 4, and 5 contain material sampled from master recordings mixed by *Seth Manchester* at *[Machines With Magnets](http://www.machineswithmagnets.com)* in Pawtucket, RI
-        & mastered by *Heba Kadry* at *[Timeless Mastering](http://www.timelessmastering.com/hebakadry/)* in Brooklyn, NY.
+        ## SOUND
+
+        Tracks 2, 3, 4, and 5 contain material sampled from master recordings mixed by *Seth Manchester* at *[Machines With Magnets](http://www.machineswithmagnets.com)* in Pawtucket, RI & mastered by *Heba Kadry* at *[Timeless Mastering](http://www.timelessmastering.com/hebakadry/)* in Brooklyn, NY.
 
         *[J. Mamana](https://jmamana.net/)* plays synthesizer on track 3 and takes a guitar solo on track 4.
-        *Matthew Marsico* plays drums on tracks 2 and 4. They also play the wurlitzer stabs on track 4.
+        *Matthew Marsico* plays drums on tracks 2 and 4.
         *Seth Manchester* engineered the drum recordings at Machines With Magnets in Pawtucket, RI.
 
         Tracks 2 and 4 feature recordings of the ARP 2500 at Brown University sequenced using *[Braid](http://braid.live/)*, written in Python by *[Brian House](https://brianhouse.net/)*.
 
-        ## WEBSITE
+        ## DESIGN
 
         *[Will Urmston](http://urmston.website)* designed and developed this website. [View source.](https://github.com/simulcast/los-angeles-ends)
-
         Jesenya Maldonado drew the homepage map.
-
-        *[David Anthony King](http://davidanthonyking.com/)* designed the Simulcast logo.
+        *[David Anthony King](http://davidanthonyking.com/)* drew the Simulcast logo.
 
         ## ACKNOWLEDGEMENTS
 
