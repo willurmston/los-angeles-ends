7f6a74c776526ca25c7187b6ab40f8943772b4ae
simulcast <thisissimulcast@gmail.com>
2019-01-08 16:03:18 -0800

    testing split up quotes across multiple slides

--- content/through-the-windshield/index.js
+++ content/through-the-windshield/index.js
@@ -41,7 +41,17 @@ module.exports = {
         {
             type: 'quote',
             markdown: markdown`
-                "Two weeks before I moved to Los Angeles I was in a brutal automobile crash — a traumatic form of epistemological break. In a sudden mangle of glass and steel, I went through the windshield. It was as if I had to go through that glass in order to live in Los Angeles. Normally, the windshield acts as a corrective lens framing the world as it reels by and as a transparent protective skin that prevents us from entering the frame. Once I found myself on the other side, inside the frame, reality and fantasy slid into each other in a peculiarly confusing blur..."
+                "Two weeks before I moved to Los Angeles I was in a brutal automobile crash — a traumatic form of epistemological break. In a sudden mangle of glass and steel, I went through the windshield. It was as if I had to go through that glass in order to live in Los Angeles..."
+
+                — Anne Friedberg, "Frame and Fame" (1992)
+            `
+        },
+        {
+            type: 'quote',
+            markdown: markdown`
+                "...Normally, the windshield acts as a corrective lens framing the world as it reels by and as a transparent protective skin that prevents us from entering the frame. Once I found myself on the other side, inside the frame, reality and fantasy slid into each other in a peculiarly confusing blur..."
+
+                — Anne Friedberg, "Frame and Fame" (1992)
             `
         },
         {
@@ -75,7 +85,15 @@ module.exports = {
         {
             type: 'quote',
             markdown: markdown`
-                "...I recuperated at my partner’s family home, one of those capacious Hancock Park houses surrounded by palm trees but designed to look like a Tudor cottage in the Cotswolds. On the other side of the backyard hedge, I was told, was the house from _Sunset Boulevard_... The back hedge of the house in _Sunset Boulevard_ was now a place where the dog barked at night as if he were calling across the lawns to ghosts that swam there thousands of nights before. _Sunset Boulevard_ was just as far and just as close as it had ever been."
+                "...I recuperated at my partner’s family home, one of those capacious Hancock Park houses surrounded by palm trees but designed to look like a Tudor cottage in the Cotswolds. On the other side of the backyard hedge, I was told, was the house from _Sunset Boulevard_..."
+
+                — Anne Friedberg, "Frame and Fame" (1992)
+            `
+        },
+        {
+            type: 'quote',
+            markdown: markdown`
+                "...The back hedge of the house in _Sunset Boulevard_ was now a place where the dog barked at night as if he were calling across the lawns to ghosts that swam there thousands of nights before. _Sunset Boulevard_ was just as far and just as close as it had ever been."
 
                 — Anne Friedberg, "Frame and Fame" (1992)
             `
@@ -105,7 +123,7 @@ module.exports = {
         {
             type: 'narration',
             markdown: markdown`
-                originally called cahuenga road no. 2, in 1885 western avenue was re-named for a cardinal boundary of the city's development.
+                originally called cahuenga road no. 2, in 1885 western avenue was re-named for a what was then cardinal boundary of the city's development.
 
                 it runs in a straight line, uninterrupted from los feliz boulevard in the hollywood hills to the pacific ocean in san pedro, the second-longest north-south street in the city.
             `
@@ -113,7 +131,14 @@ module.exports = {
         {
             type: 'quote',
             markdown: markdown`
-                "First, there was Hollywood, the former village turned suburb turned part of Los Angeles — a specific place with geographic boundaries, constantly changing and contested. Then there was Hollywood as a generally accepted name for the homeland of the American movie industry... a shorthand for the stars and the films. Then, allied to that sense of phsyical place was yet another Hollywood, a more or less intangible place on earth or in the atmosphere above it, to which aspirants to fame could come and be recognized."
+                "First, there was Hollywood, the former village turned suburb turned part of Los Angeles — a specific place with geographic boundaries, constantly changing and contested. Then there was Hollywood as a generally accepted name for the homeland of the American movie industry..."
+                — Leo Braudy, *The Hollywood Sign* (2011)
+            `
+        },
+        {
+            type: 'quote',
+            markdown: markdown`
+                "...Then, allied to that sense of phsyical place was yet another Hollywood, a more or less intangible place on earth or in the atmosphere above it, to which aspirants to fame could come and be recognized."
                 — Leo Braudy, *The Hollywood Sign* (2011)
             `
         },
